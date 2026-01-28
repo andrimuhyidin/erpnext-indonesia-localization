56eb9546b933a7ca4d00ec810bfa68cf20a13ed7
Andri Muhyidin <andrimuhyidin55@gmail.com>
2026-01-29 01:29:36 +0700

    fix: add missing Python files for logistics DocTypes
    Added __init__.py and controller files for:
    - Indonesia Logistics Settings
    - Logistics Courier

---
+++ erpnext_indonesia_localization/erpnext_indonesia_localization/doctype/indonesia_logistics_settings/indonesia_logistics_settings.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2026, BIZOPS and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class IndonesiaLogisticsSettings(Document):
+	"""
+	Indonesia Logistics Settings DocType.
+	
+	Stores configuration for Indonesian logistics/shipping integrations.
+	"""
+	pass
---
+++ erpnext_indonesia_localization/erpnext_indonesia_localization/doctype/logistics_courier/logistics_courier.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2026, BIZOPS and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class LogisticsCourier(Document):
+	"""
+	Logistics Courier DocType for Indonesian shipping providers.
+	
+	Stores courier/shipping provider information for logistics integration.
+	"""
+	pass
